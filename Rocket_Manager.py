d9afd193b7961d2bc7027fab449178b1184f0632
Sergio Brighenti <sergio@brighenti.me>
2016-07-15 16:22:52 +0200

    linux support improved path building fixed flags not resetted after first cicle

--- Rocket_Manager.py
+++ Rocket_Manager.py
@@ -10,21 +10,28 @@ from xml.etree import ElementTree
 
 # --Constants
 
-MANAGER_FOLDER = "_RocketManager\\"
+MANAGER_FOLDER = "RocketManager"
+STEAM_FOLDER = "steamcmd"
+TMP_FOLDER = "temp"
+UNTURNED_PATH = "unturned"
 
-ROCKET_EXTRACT_FOLDER = MANAGER_FOLDER + "last_rocket_download"
-BACKUP_BUNDLES_FOLDER = MANAGER_FOLDER + "cpy_bundles"
+ROCKET_EXTRACT_FOLDER = os.path.join(MANAGER_FOLDER, "last_rocket_download")
+BACKUP_BUNDLES_FOLDER = os.path.join(MANAGER_FOLDER, "bundles_backup")
+
+STEAM_EXECUTABLE = "steamcmd.exe" if platform.system() == "Windows" else "steamcmd.sh"
+STEAM_EXECUTABLE = os.path.join(STEAM_FOLDER, STEAM_EXECUTABLE)
 
 # For Win
-URL_ROCKET_BETA = "http://api.rocket.foundation/download/unturned/latest/"
+URL_ROCKET_BETA = "http://api.rocketmod.net/download/unturned/latest/"
 URL_STEAM_WIN = "http://media.steampowered.com/installer/steamcmd.zip"
 
-OUTPUT_ZIP_STEAM_WIN = MANAGER_FOLDER + "steam_temp.zip"
-OUTPUT_ZIP_ROCKET = MANAGER_FOLDER + "rocket_temp.zip"
+OUTPUT_ZIP_STEAM_WIN = os.path.join(TMP_FOLDER, "steam_temp.zip")
+OUTPUT_ZIP_STEAM_LINUX = os.path.join(TMP_FOLDER, "steamcmd_temp.tar.gz")
+OUTPUT_ZIP_ROCKET = os.path.join(TMP_FOLDER, "rocket_temp.zip")
 
 # For Linux
 
-URL_ROCKET_LINUX = "http://api.rocket.foundation/download/unturned-linux/latest/"
+URL_ROCKET_LINUX = "http://api.rocketmod.net/download/unturned-linux/latest/"
 URL_STEAM_LINUX = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
 
 PROCNAME_WIN = "Unturned.exe"
@@ -36,7 +43,7 @@ def write_config(name):
     f.write('''<?xml version="1.0" encoding="UTF-8"?>
 <config>
 	<rebootEvery seconds="3600" />
-	<unturnedFolder path=".\unturned" recoveryBundlesAfterUpdates="false" />
+	<unturnedFolder recoveryBundlesAfterUpdates="false" />
 	<rocket useRocket="true" apikey=""/>
 	<steam username="" password="" />
 	<steamUpdates validate="true" />
@@ -56,7 +63,6 @@ def load_config(name):
     global RCON_ENABLED
     global RCON_PORT
     global RCON_PASSWORD
-    global UNTURNED_PATH
     global VALIDATE_AT_BOOT
     global STEAM_USER
     global STEAM_PASS
@@ -78,7 +84,6 @@ def load_config(name):
         VALIDATE_AT_BOOT = node.attrib.get("validate")
 
         node = tree.find("unturnedFolder")
-        UNTURNED_PATH = node.attrib.get("path")
         BACKUP_BUNDLES = node.attrib.get("recoveryBundlesAfterUpdates")
         if VALIDATE_AT_BOOT != "true":
             BACKUP_BUNDLES = "false"
@@ -120,7 +125,7 @@ def downloader(i):
             if platform.system() == "Windows":
                 urllib.urlretrieve(URL_STEAM_WIN, OUTPUT_ZIP_STEAM_WIN)
             else:
-                urllib.urlretrieve(URL_STEAM_LINUX, OUTPUT_ZIP_STEAM_WIN)
+                urllib.urlretrieve(URL_STEAM_LINUX, OUTPUT_ZIP_STEAM_LINUX)
 
         except:
             err = True
@@ -144,15 +149,29 @@ def extractor(namezip, folder):
     zfile.close()
 
 
+def test_zip(namezip):
+    try:
+        zzip = zipfile.ZipFile(namezip)
+        checkzip = zzip.testzip()
+        zzip.close()
+        if checkzip is None:
+            return True
+        else:
+            return False
+    except zipfile.BadZipfile:
+        return False
+
+
 def clean_up():
-    try:
-        os.remove(OUTPUT_ZIP_STEAM_WIN)
-    except:
-        pass
-    try:
-        os.remove(OUTPUT_ZIP_ROCKET)
-    except:
-        pass
+    for the_file in os.listdir(TMP_FOLDER):
+        file_path = os.path.join(TMP_FOLDER, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception:
+            pass
 
 
 def installer(folder):
@@ -160,8 +179,16 @@ def installer(folder):
         for f in os.listdir(folder):
             if not os.path.isdir(os.path.join(folder, f)):
                 src_file = os.path.join(folder, f)
-                dst_file = os.path.join(UNTURNED_PATH + "\\Unturned_Data\\Managed\\", f)
+                if platform.system() == "Windows":
+                    dst_file = os.path.join(UNTURNED_PATH, "Unturned_Data", "Managed", f)
+                else:
+                    dst_file = os.path.join(UNTURNED_PATH, "Unturned_Headless_Data", "Managed", f)
                 shutil.copyfile(src_file, dst_file)
+        if platform.system() != "Windows":
+            shutil.copyfile(os.path.join(folder, "Scripts", "start.sh"), os.path.join(".", "start.sh"))
+            shutil.copyfile(os.path.join(folder, "RocketLauncher.exe"), os.path.join(UNTURNED_PATH, "RocketLauncher.exe"))
+            os.system("chmod 755 " + os.path.join(".", "start.sh"))
+            os.system("chmod 755 " + os.path.join(UNTURNED_PATH, "RocketLauncher.exe"))
         return False
     except IOError:
         return True
@@ -230,24 +257,63 @@ def rcon_shutdown(port, passw):
 def kill_server(item=None):
     if platform.system() == "Windows":
         os.system("taskkill /f /im " + PROCNAME_WIN)
+    else:
+        os.system("screen -S " + item + ' -X stuff "save^M"')
+
+
+def start_server(server):
+    if platform.system() == "Windows":
+        os.system("cd " + UNTURNED_PATH + "\ & start Unturned.exe -nographics -batchmode -silent-crashes +secureserver/" + server)
+    else:
+        os.system("screen -dmS " + server + " ./start.sh " + server)
+
+
+def steamcmd_run():
+    print ("------------------------------------SteamCMD------------------------------------\n")
+    if platform.system() == "Windows":
+        os.system(STEAM_EXECUTABLE + " +login " + STEAM_USER + " " + STEAM_PASS + " +force_install_dir " + os.path.join("..", UNTURNED_PATH) + " +app_update 304930 validate +exit")
+    else:
+        os.system(STEAM_EXECUTABLE + " +@sSteamCmdForcePlatformBitness 32 \
+         +login " + STEAM_USER + " " + STEAM_PASS + " +force_install_dir " + os.path.join("..", UNTURNED_PATH) + " +app_update 304930 validate +exit")
+    print ("\n------------------------------------END-----------------------------------------\n\n")
+
+
+def bundles(mode):
+    try:
+        if mode == "save":
+            if not os.path.exists(BACKUP_BUNDLES_FOLDER):
+                os.makedirs(BACKUP_BUNDLES_FOLDER)
+            bundles_folder = os.path.join(BACKUP_BUNDLES_FOLDER, "Bundles")
+            if os.path.exists(bundles_folder):
+                shutil.rmtree(bundles_folder)
+            shutil.copytree(os.path.join(UNTURNED_PATH, "Bundles"), bundles_folder)
+            return False
+        if mode == "restore":
+            return merge_files(os.path.join(BACKUP_BUNDLES_FOLDER, "Bundles"), os.path.join(UNTURNED_PATH, "Bundles"))
+    except Exception:
+        return True
 
 
 def bootstrap():
     print("--------------------------------------------------------------------------------")
-    print("                          SergiX44's Rocket Manager 1.8.4                       ")
+    print("                          SergiX44's Rocket Manager 1.9                         ")
     print("--------------------------------------------------------------------------------\n\n")
 
-    print("> Creating folders...")
+    print("> Checking folders...")
     if not os.path.exists(MANAGER_FOLDER):
         os.makedirs(MANAGER_FOLDER)
+    if not os.path.exists(STEAM_FOLDER):
+        os.makedirs(STEAM_FOLDER)
+    if not os.path.exists(TMP_FOLDER):
+        os.makedirs(TMP_FOLDER)
 
     print("> Loading config...")
-    if load_config(MANAGER_FOLDER + "config_RocketManager.xml"):
+    if load_config(os.path.join(MANAGER_FOLDER, "config_RocketManager.xml")):
         print("Close and edit config_RocketManager.xml, then restart me!")
         raw_input("Press any key to continue...")
         sys.exit(1)
 
-    if not os.path.isfile("steamcmd.exe"):
+    if not os.path.isfile(STEAM_EXECUTABLE):
         ex = True
         while ex:
             sel = raw_input("> SteamCMD not found! Would you like download it? (y/n) ")
@@ -258,29 +324,35 @@ def bootstrap():
                     raw_input("Press any key to continue...")
                     sys.exit(3)
 
-                zfile = zipfile.ZipFile(OUTPUT_ZIP_STEAM_WIN)
-                zfile.extractall()
-                zfile.close()
+                if platform.system() == "Windows":
+                    extractor(OUTPUT_ZIP_STEAM_WIN, STEAM_FOLDER)
+                else:
+                    os.system("tar -xzf " + OUTPUT_ZIP_STEAM_LINUX + " -C " + STEAM_FOLDER)
+                    os.system("chmod 755 " + STEAM_EXECUTABLE)
+                    os.system("dpkg --add-architecture i386")
+                    os.system("apt-get update")
+                    os.system("apt-get -y install libmono2.0-cil mono-runtime screen htop unzip lib32gcc1 lib32stdc++6 libglu1-mesa libxcursor1 libxrandr2 libc6:i386 libgl1-mesa-glx:i386 libxcursor1:i386 libxrandr2:i386")
                 ex = False
             if sel == "n":
                 ex = False
                 print("Closing...")
                 time.sleep(1)
                 sys.exit(1)
+    if (platform.system() != "Windows") and ROCKET_ENABLED == "false":
+        print("ERROR: This tool can run under Linux only with Rocket enabled, due a bug with the Unturned console. Please enable rocket.")
+        sys.exit(100)
 
 
 def main():
-    rocket_installed = True
-    rocket_downloaded = True
-    correct_opened = True
-    checkzip = None
-
     bootstrap()
 
     while 1:
+        rocket_installed = True
+        rocket_downloaded = True
+
         # reloading config
         print("> Reloading config...")
-        if load_config(MANAGER_FOLDER + "config_RocketManager.xml"):
+        if load_config(os.path.join(MANAGER_FOLDER, "config_RocketManager.xml")):
             print("> Failed loading config. \n"
                   "Config file regenerated, edit config_RocketManager.xml, then restart.")
             raw_input("Press any key to continue...")
@@ -289,26 +361,18 @@ def main():
         # saving bundles
         if BACKUP_BUNDLES == "true":
             print("> Saving Bundles...")
-            try:
-                if not os.path.exists(BACKUP_BUNDLES_FOLDER):
-                    os.makedirs(BACKUP_BUNDLES_FOLDER)
-                if os.path.exists(BACKUP_BUNDLES_FOLDER + "\\Bundles"):
-                    shutil.rmtree(BACKUP_BUNDLES_FOLDER + "\\Bundles")
-                shutil.copytree(UNTURNED_PATH + "\\Bundles", BACKUP_BUNDLES_FOLDER + "\\Bundles")
-            except:
+            if bundles("save"):
                 print("ERROR: Cannot saving Bundles, aborting...")
 
         # launch steam cmd
         if (not os.path.isdir(UNTURNED_PATH)) or (VALIDATE_AT_BOOT == "true"):
             print("> Launching SteamCMD...")
-            print ("------------------------------------SteamCMD------------------------------------\n")
-            os.system("steamcmd.exe +login " + STEAM_USER + " " + STEAM_PASS + " +force_install_dir " + UNTURNED_PATH + " +app_update 304930 validate +exit")
-            print ("\n------------------------------------END-----------------------------------------\n\n")
+            steamcmd_run()
 
         # recovering bundles
         if BACKUP_BUNDLES == "true":
             print("> Recovering Bundles...")
-            if merge_files(BACKUP_BUNDLES_FOLDER + "\\Bundles", UNTURNED_PATH + "\\Bundles"):
+            if bundles("restore"):
                 print("ERROR: Cannot recovering Bundles, aborting...")
 
         if ROCKET_ENABLED == "true":
@@ -321,13 +385,7 @@ def main():
 
             # extract
             print("> Extracting rocket...")
-            try:
-                zzip = zipfile.ZipFile(OUTPUT_ZIP_ROCKET)
-                checkzip = zzip.testzip()
-                zzip.close()
-            except zipfile.BadZipfile:
-                correct_opened = False
-            if (checkzip is None) and correct_opened:
+            if test_zip(OUTPUT_ZIP_ROCKET):
                 if os.path.exists(ROCKET_EXTRACT_FOLDER):
                     shutil.rmtree(ROCKET_EXTRACT_FOLDER)
                 extractor(OUTPUT_ZIP_ROCKET, ROCKET_EXTRACT_FOLDER)
@@ -360,7 +418,7 @@ def main():
             print("> Launching servers...")
             for i in range(0, len(SERVERS_TO_LAUNCH)):
                 print("    - Launching " + SERVERS_TO_LAUNCH[i])
-                os.system("cd " + UNTURNED_PATH + "\ & start Unturned.exe -nographics -batchmode -silent-crashes +secureserver/" + SERVERS_TO_LAUNCH[i])
+                start_server(SERVERS_TO_LAUNCH[i])
                 time.sleep(1)
 
             # timer
